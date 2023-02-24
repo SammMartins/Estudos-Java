762901f2721629223ee90287465432381d217edd
SAMMUEL GUSMÃO MARTINS <sammuel.martins+github@ftc.edu.br>
2023-02-23 21:05:55 -0300

    Iniciando
    Primeiros programas feitos em Java, na primeira semana do semestre.

---
+++ Aula 1/CalcDesc.java
@@ -0,0 +1,26 @@
+
+/*Usando a classe BufferedReader para entrada de dados, 
+crie uma classe que receba o valor de um produto e a percentagem de desconto, 
+calcule e mostre o valor do desconto e o valor do produto com o desconto. 
+Observação: o valor do desconto é calculado por meio da fórmula:
+valor do desconto = valor do produto * percentual de desconto /100 */
+
+import java.util.Scanner;
+public class CalcDesc{
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in); //Entrada padrão do sistema
+        
+        float valor, desconto,valorDesc;
+
+        System.out.print("Digite o valor do produto: "); //Saída padrão java
+        valor = entrada.nextFloat();
+
+        System.out.print("Digite a porcentagem do desconto: "); //Saída padrão java
+        desconto = entrada.nextFloat();
+        
+        valorDesc = valor - (valor * desconto/100);
+        System.out.println("O valor do desconto foi: R$"+ (valor * desconto/100));
+        System.out.print("O valor do Produto com desconto é: R$" + valorDesc);
+
+    }
+}
---
+++ Aula 1/Outros/fut.java
@@ -0,0 +1,30 @@
+package Outros;
+
+/*Usando a classe Scanner para entrada de dados, 
+faça uma classe que receba dois valores inteiro. 
+O primeiro valor corresponde à quantidade de pontos do líder do campeonato brasileiro de futebol. 
+O segundo valor corresponde à quantidade de pontos do time lanterna.
+Considerando que cada vitória vale 3 pontos e que o líder não ganhará mais nenhuma partida, 
+elabore uma classe que calcule o número de vitórias necessárias para que o time lanterna alcance 
+(ou ultrapasse) o líder*/
+
+import java.util.Scanner;
+class fut{
+    
+    public static void main(String args[]) {
+        
+        Scanner teclado = new Scanner(System.in);
+            int lider,lanterna,vit;
+
+            System.out.print("Digite a pontuaçao do Lider: ");
+            lider = teclado.nextInt();
+
+            System.out.print("Digite a pontuaçao do Lanterna: ");
+            lanterna = teclado.nextInt();
+
+            vit = (lider - lanterna)/3;
+
+            System.out.println("Faltam " + vit + " vitorias.");
+        
+    }
+}
