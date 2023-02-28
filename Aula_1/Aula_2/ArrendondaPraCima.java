8a6c63c580c11e2971ed5cb9b4821259ff98f7de
SAMMUEL GUSMÃO MARTINS <118392533+SammMartins@users.noreply.github.com>
2023-02-27 22:28:04 -0300

    Aula semana 3

---
+++ Aula_1/Aula_2/ArrendondaPraCima.java
@@ -0,0 +1,22 @@
+package AULA_2;
+import javax.swing.JOptionPane;
+// O `.` (ponto) define hierarquia de pastas em java
+
+public class ArrendondaPraCima{
+    public static void main(String args[]){
+        final float LOTACAO_ONIBUS = 50; // `final` define uma contante no código
+        float qtdOnibus;
+        String s;
+        int numeroPassageiros;
+        
+        s = JOptionPane.showInputDialog("quantos passageiros?");
+        numeroPassageiros = Integer.parseInt(s);
+
+        qtdOnibus = numeroPassageiros / LOTACAO_ONIBUS;
+
+        JOptionPane.showMessageDialog(null,
+            "Quantidade de passageiros = " + numeroPassageiros + "\n" +
+            "Quantidade de Ônibus calculada = " + qtdOnibus + "\n" +
+            "Quantidade de Ônibus necessária = " + (int) Math.ceil(qtdOnibus));//Arrendonda SEMPRE para MAIS 
+    }
+}
