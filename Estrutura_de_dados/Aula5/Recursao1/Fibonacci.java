52870a23aa1c8b45c714f187f5a21e52d9808b57
SAMMUEL GUSMÃO MARTINS <sammuel.martins+github@ftc.edu.br>
2023-04-30 11:26:30 -0300

    Recursão com Fibonacci
    Adicionando classe Fibonacci que calcula a sequência de Fibonacci recursivamente.
    
    A classe Fibonacci possui um método calc que recebe um inteiro i como parâmetro e retorna o i-ésimo número da sequência de Fibonacci. O cálculo é realizado de forma recursiva, utilizando as variáveis N, antN e v. A variável N armazena o número atual da sequência, antN armazena o número anterior a N e v armazena o valor de N antes do cálculo. A variável o é utilizada para controlar a posição na sequência.
    
    O método realiza uma recursão enquanto a posição da sequência não for alcançada e, quando a posição é alcançada, retorna o valor de N.

--- Estrutura_de_dados/Aula5/Recursao1/Fibonacci.java
+++ Estrutura_de_dados/Aula5/Recursao1/Fibonacci.java
@@ -1,14 +1,33 @@
 package Estrutura_de_dados.Aula5.Recursao1;
 
 public class Fibonacci {
-    private int n = 1;
+    private int v; // Variável que armazena o valor anterior de N
+    private int N = 1; // Variável que armazena o número atual da sequencia de fibonacci
+    private int o = 3; // Variável que armazena a posição de N na sequencia de fibonacci
+    private int antN = 1; // Variável que armazena o número anterior a N na sequencia de fibonacci
 
     public int calc(int i) {
-        int o=1;
-        n = (n - 1) + (n-2);
-        if(o!=i){
-            o++; calc(i);
-        } 
-        return n;
+        v = N; // Recebe o valor de N antes do cálculo
+        N = antN + N; // N recebe o valor seguinte da sequencia de fibonacci
+        antN = v; // Recebe o valor de N antes do cálculo através de v
+        if (o < i) {
+            o++;
+            calc(i); // Realiza a recursão enquanto a posição da sequencia não for alcançada
+        } else
+            return N;
+        return N;
     }
-} // F(n + 2) = F(n + 1) + F(n)
+}
+/*
+ * Adicionando classe Fibonacci que calcula a sequência de Fibonacci recursivamente.
+ * 
+ * A classe Fibonacci possui um método calc que recebe um inteiro i como
+ * parâmetro e retorna o i-ésimo número da sequência de Fibonacci. O cálculo é
+ * realizado de forma recursiva, utilizando as variáveis N, antN e v. A variável
+ * N armazena o número atual da sequência, antN armazena o número anterior a N e
+ * v armazena o valor de N antes do cálculo. A variável o é utilizada para
+ * controlar a posição na sequência.
+ * 
+ * O método realiza uma recursão enquanto a posição da sequência não for
+ * alcançada e, quando a posição é alcançada, retorna o valor de N.
+ */
--- Estrutura_de_dados/Aula5/Recursao1/Main.java
+++ Estrutura_de_dados/Aula5/Recursao1/Main.java
@@ -1,9 +1,17 @@
 package Estrutura_de_dados.Aula5.Recursao1;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String args[]) {
         Fibonacci fibonacci = new Fibonacci();
-        System.out.print(fibonacci.calc(15));
+        Scanner input = new Scanner(System.in);
+        int x;
+        System.out.print("Número desejado da sequência de Fibonacci: ");
+        x = input.nextInt();
+
+        System.out.print("O número " + x + "º da sequência de Fibonacci é " + fibonacci.calc(x));
+        input.close();
     }
-    
+
 }
