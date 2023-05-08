e7269bc13586f95152bd356091aaf1a2e2b13e86
SAMMUEL GUSMÃO MARTINS <118392533+SammMartins@users.noreply.github.com>
2023-05-08 15:21:43 -0300

    Update ArvoreBinaria.java

--- Estrutura_de_dados/Aula6/ArvoreBinaria.java
+++ Estrutura_de_dados/Aula6/ArvoreBinaria.java
@@ -16,11 +16,11 @@ class No {
 }
 
 /* Em seguida, definimos a classe ArvoreBinaria que contém o método main como
- * ponto de entrada do programa. */
+   ponto de entrada do programa. */
 public class ArvoreBinaria {
 
     /* Dentro do método main, criamos os nós da árvore em ordem aleatória,
-     * atribuindo a cada nó um valor.*/
+       atribuindo a cada nó um valor.*/
     public static void main(String[] args) {
         // Criando os nós da árvore em ordem aleatória:
         No raiz = new No(4);
@@ -32,9 +32,9 @@ public class ArvoreBinaria {
         No no7 = new No(7);
 
         /* Em seguida, montamos a estrutura da árvore, estabelecendo as relações entre os nós.
-         * Definimos que o nó 2 é filho esquerdo da raiz, o nó 3 é filho direito da
-         * raiz, o nó 4 é filho esquerdo do nó 2, o nó 5 é filho direito do nó 2, o nó 6 é filho
-         * esquerdo do nó 3 e o nó 7 é filho direito do nó 3. */
+           Definimos que o nó 2 é filho esquerdo da raiz, o nó 3 é filho direito da
+           raiz, o nó 4 é filho esquerdo do nó 2, o nó 5 é filho direito do nó 2, o nó 6 é filho
+           esquerdo do nó 3 e o nó 7 é filho direito do nó 3. */
         raiz.esquerda = no2; // Nó 2 é filho esquerdo da raiz
         raiz.direita = no3; // Nó 3 é filho direito da raiz
         no2.esquerda = no4; // Nó 4 é filho esquerdo do nó 2
@@ -43,9 +43,9 @@ public class ArvoreBinaria {
         no3.direita = no7; // Nó 7 é filho direito do nó 3
 
         /* Após montar a estrutura da árvore, chamamos o método percorrerEmOrdem
-         * passando a raiz como argumento. Esse método percorre a árvore em ordem, ou
-         * seja, primeiro visita o filho esquerdo, depois o nó atual e por último o
-         * filho direito.*/
+           passando a raiz como argumento. Esse método percorre a árvore em ordem, ou
+           seja, primeiro visita o filho esquerdo, depois o nó atual e por último o
+           filho direito.*/
         System.out.println("Elementos da árvore em ordem:");
         percorrerEmOrdem(raiz);
     }
@@ -53,16 +53,16 @@ public class ArvoreBinaria {
     public static void percorrerEmOrdem(No no) {
 
         /* Dentro do método percorrerEmOrdem, verificamos se o nó passado
-         * como parâmetro não é nulo. Se não for nulo, chamamos recursivamente o método
-         * percorrerEmOrdem passando o filho esquerdo do nó.*/
+           como parâmetro não é nulo. Se não for nulo, chamamos recursivamente o método
+           percorrerEmOrdem passando o filho esquerdo do nó.*/
         if (no != null) {
             percorrerEmOrdem(no.esquerda); // Percorre o filho esquerdo
             System.out.print(no.valor + " "); // Imprime o valor do nó
             /* depois disso, chamamos recursivamente o método percorrerEmOrdem passando o
-             * filho direito do nó.*/
+               filho direito do nó.*/
             percorrerEmOrdem(no.direita); // Percorre o filho direito
         }
         /* Por fim, o programa imprime os elementos da árvore em ordem, ou seja, imprime
-         * os valores dos nós na sequência correta.*/
+           os valores dos nós na sequência correta.*/
     }
 }
