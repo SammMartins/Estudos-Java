34072419c517c7bfdcc2fcb9f117a366a1a1e116
SAMMUEL GUSMÃO MARTINS <sammuel.martins+github@ftc.edu.br>
2023-05-13 18:40:38 -0300

    Update

--- Estrutura_de_dados/Aula7_Projeto/TorreHanoi.java
+++ Estrutura_de_dados/Aula7_Projeto/TorreHanoi.java
@@ -1,5 +1,43 @@
 package Estrutura_de_dados.Aula7_Projeto;
-
 public class TorreHanoi {
+    public static void main(String[] args) {
+        int discos = 6;
+        char torreOriginal = 'A';
+        char torreDestino = 'C';
+        char hasteAuxiliar = 'B';
+
+        System.out.println("Resolvendo a Torre de Hanói com " + discos + " discos:");
+
+        resolverTorreDeHanoi(discos, torreOriginal, torreDestino, hasteAuxiliar);
+    }
+
+    public static void resolverTorreDeHanoi(int discos, char hasteOrigem, char hasteDestino, char hasteAuxiliar) {
+        if (discos == 1) {
+            digitar("Mover disco 1 de " + hasteOrigem + " para " + hasteDestino);
+            return;
+        }
+
+        resolverTorreDeHanoi(discos - 1, hasteOrigem, hasteAuxiliar, hasteDestino);
+        digitar("Mover disco " + discos + " de " + hasteOrigem + " para " + hasteDestino);
+        pausa(3000);
+        System.out.println("\n");
+        resolverTorreDeHanoi(discos - 1, hasteAuxiliar, hasteDestino, hasteOrigem);
+    }
     
+    public static void digitar(String texto) {
+        int velocidade = 90; // Tempo de pausa entre cada caractere (em milissegundos)
+        for (int i = 0; i < texto.length(); i++) {
+            System.out.print(texto.charAt(i));
+            pausa(velocidade);
+        }
+        System.out.println();
+    }
+
+    public static void pausa(int milissegundos) {
+        try {
+            Thread.sleep(milissegundos);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
 }
